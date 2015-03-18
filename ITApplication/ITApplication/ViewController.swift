c6bf016a1f21440e33ef5e4f163b5ef182011c75
55011212043 <istudents@iClassrooms-iMac-8.local>
2015-03-18 14:23:05 +0700

    แอปแนะนำคณะ

---
+++ ITApplication/ITApplication/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ITApplication
+//
+//  Created by iStudents on 3/6/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate {
+
+    @IBOutlet weak var head: UIImageView!
+    @IBOutlet weak var news1: UITextView!
+    @IBOutlet weak var news2: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+
+}
+
